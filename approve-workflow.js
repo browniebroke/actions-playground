bbb9d1739c9587517924e2bc3cc4c07ae6f1cc9c
Bruno Alla <browniebroke@users.noreply.github.com>
2021-06-02 19:18:35 +0100

    Wrap in immediately-invoked async function

--- approve-workflow.js
+++ approve-workflow.js
@@ -1,20 +1,22 @@
 const { Octokit } = require("@octokit/action");
 
-const octokit = new Octokit();
-const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+(async function() {
+  const octokit = new Octokit();
+  const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
 
 // See https://developer.github.com/v3/issues/#create-an-issue
-const { data } = await octokit.request("GET /repos/{owner}/{repo}/actions/runs", {
-  owner,
-  repo,
-  event: "pull_request",
-});
-console.log("Data: %s", data);
+  const {data} = await octokit.request("GET /repos/{owner}/{repo}/actions/runs", {
+    owner,
+    repo,
+    event: "pull_request",
+  });
+  console.log("Data: %s", data);
 
 
-/*
-with:
-  route: GET /repos/{repository}/actions/runs/{run_id}/approve
-  repository: ${{ github.repository }}
-  run_id: ${{ github.event.check_suite.id }}
- */
+  /*
+  with:
+    route: GET /repos/{repository}/actions/runs/{run_id}/approve
+    repository: ${{ github.repository }}
+    run_id: ${{ github.event.check_suite.id }}
+   */
+}());
