e6ec51ce1f7abef078a5523f7b20deaf1512a0b7
Bruno Alla <browniebroke@users.noreply.github.com>
2021-06-02 19:51:56 +0100

    Simplify

--- approve-workflow.js
+++ approve-workflow.js
@@ -12,19 +12,11 @@ const { Octokit } = require("@octokit/action");
   console.log(data);
 
   const unfinished_runs = data.workflow_runs.filter(run => run.conclusion === null)
-  const approval_promises = unfinished_runs.map((run) => {
-    console.log("Name: %s", run.name);
-    console.log("Status: %s", run.status);
-    console.log("Branch: %s", run.head_branch);
-    console.log("ID: %s", run.id);
-    console.log("Conclusion: %s", run.conclusion);
-    return octokit.request("POST /repos/{repository}/actions/runs/{run_id}/approve", {
+  for(const run of unfinished_runs) {
+    console.log(run);
+    await octokit.request("POST /repos/{repository}/actions/runs/{run_id}/approve", {
       repository,
       run_id: run.id
     });
-  })
-  await Promise.all(approval_promises).catch(error=>{
-    console.error(error)
-    process.exit(1);
-  });
+  }
 }());
