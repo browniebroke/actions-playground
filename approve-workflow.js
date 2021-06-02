0136d6978eb99427d3aa50747d1adb59516dc986
Bruno Alla <browniebroke@users.noreply.github.com>
2021-06-02 19:43:56 +0100

    Approve workflow runs

--- approve-workflow.js
+++ approve-workflow.js
@@ -2,21 +2,26 @@ const { Octokit } = require("@octokit/action");
 
 (async function() {
   const octokit = new Octokit();
-  const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+  const repository = process.env.GITHUB_REPOSITORY.split("/");
 
 // See https://developer.github.com/v3/issues/#create-an-issue
-  const {data} = await octokit.request("GET /repos/{owner}/{repo}/actions/runs", {
-    owner,
-    repo,
+  const {data} = await octokit.request("GET /repos/{repository}/actions/runs", {
+    repository,
     event: "pull_request",
   });
-  console.log("Data: %s", data);
+  console.log(data);
 
-
-  /*
-  with:
-    route: GET /repos/{repository}/actions/runs/{run_id}/approve
-    repository: ${{ github.repository }}
-    run_id: ${{ github.event.check_suite.id }}
-   */
+  const unfinished_runs = data.workflow_runs.filter(run => run.conclusion === null)
+  const approval_promises = unfinished_runs.map((run) => {
+    console.log("Name: %s", run.name);
+    console.log("Status: %s", run.status);
+    console.log("Branch: %s", run.head_branch);
+    console.log("ID: %s", run.id);
+    console.log("Conclusion: %s", run.conclusion);
+    return octokit.request("POST /repos/{repository}/actions/runs/{run_id}/approve", {
+      repository,
+      run_id: run.id
+    });
+  })
+  await Promise.all(approval_promises);
 }());
